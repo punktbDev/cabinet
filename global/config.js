daaa76dbd514033d7e288f9ad86a406706136f67
Пункт Б: Разработка <145120621+punktbDev@users.noreply.github.com>
2025-07-16 00:55:37 +0300

    Update config.js

--- global/config.js
+++ global/config.js
@@ -1,2 +1,2 @@
-const API_URL = "https://vds.punkt-b.pro/api/v1"
-const DIAGNOSTICS_URL = "https://diag.punkt-b.pro"
+const API_URL = "https://punktbdev-punktb-ea39.twc1.net/api/v1"
+const DIAGNOSTICS_URL = "https://punktbdev-diagnostics-1e36.twc1.net"
