6f1614ed05d781be40ccffb6b3da9a736bc84a33
Пункт Б: Разработка <145120621+punktbDev@users.noreply.github.com>
2025-07-26 11:42:30 +0300

    Update accessModal.js
    intdata.ru

--- admin/scripts/accessModal.js
+++ admin/scripts/accessModal.js
@@ -1,4 +1,4 @@
 $("#open-card-conclusion").on("click tap", () => {
     // Передаем данные для аутентификации
-    window.open("https://conclusion.punkt-b.pro?auth=" + btoa(userData.login + ":" + userData.password), "_blank")
+    window.open("https://conclusion.intdata.ru?auth=" + btoa(userData.login + ":" + userData.password), "_blank")
 })
